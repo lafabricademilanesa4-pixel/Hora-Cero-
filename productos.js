d39b09593328a5b8f983720de6bba3da9d44dab0
lafabricademilanesa4-pixel <lafabricademilanesa4@gmail.com>
2026-06-04 02:26:32 -0300

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,28 @@
+const MIS_PRODUCTOS = {
+    "modal-bikinis": [
+        {
+            nombre: "Bikini Coral Reef",
+            precio: 1490,
+            descripcion: "Bikini de dos piezas con detalles calados, ideal para el verano.",
+            imagen: "https://i.ibb.co/Dfk0Lh7S/IMG-20260529-230316.jpg"
+        }
+    ],
+    
+    "modal-medias": [
+        {
+            nombre: "Zoquetes Algodón Premium",
+            precio: 290,
+            descripcion: "Pack x3 de zoquetes invisibles de algodón peinado súper suave.",
+            imagen: "https://i.ibb.co/bjmQQ6Gv/IMG-20260529-230231.jpg"
+        }
+    ],
+
+    "modal-sostenes": [
+        {
+            nombre: "Sostén Encaje Eternity",
+            precio: 890,
+            descripcion: "Sostén con taza soft y detalles en encaje fino ajustable.",
+            imagen: "https://i.ibb.co/VYxFXgNr/IMG-20260529-230255.jpg"
+        }
+    ]
+};
